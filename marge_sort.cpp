9f91e87f20aa416fa7f9ff463641a6291029d651
Hasan Bijoy <mrhbijoy@gmail.com>
2024-01-21 18:20:58 +0600

    Update marge_sort.cpp

--- marge_sort.cpp
+++ marge_sort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void Marge(int Arr[],int p,int q,int r){
+void Merge(int Arr[],int p,int q,int r){
     int n1=q-p+1;
     int n2=r-q;
     int L[n1+1],R[n2+1];
@@ -25,12 +25,12 @@ void Marge(int Arr[],int p,int q,int r){
         }
     }
 }
-int Marge_sort(int Arr[],int p,int r){
+int Merge_sort(int Arr[],int p,int r){
     if(p<r){
         int q=(p+r)/2;
-        Marge_sort(Arr,p,q);
-        Marge_sort(Arr,q+1,r);
-        Marge(Arr,p,q,r);
+        Merge_sort(Arr,p,q);
+        Merge_sort(Arr,q+1,r);
+        Merge(Arr,p,q,r);
     }
 }
 
@@ -40,7 +40,7 @@ int main(){
     int Arr[n];
     for(int i=0;i<n;i++)cin>>Arr[i];
     
-    Marge_sort(Arr,0,n-1);
-    cout<<"After Marge Sort:"<<endl;
+    Merge_sort(Arr,0,n-1);
+    cout<<"After Merge Sort:"<<endl;
     for(int i=0;i<n;i++)cout<<Arr[i]<<" ";
 }
