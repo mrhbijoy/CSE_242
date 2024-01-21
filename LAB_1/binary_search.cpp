23cec2a1b73d3cb6684c851212ff8d0f238324c7
Hasan Bijoy <mrhbijoy@gmail.com>
2024-01-21 11:17:50 +0000

    marge_sort

--- LAB_1/binary_search.cpp
+++ LAB_1/binary_search.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int binary_search(int arr[],int n,int key){
-    int s=0;
-    int e=n-1;
+    int s=1;
+    int e=n;
     while(s<=e){
         
         int mid=(s+e)/2;
